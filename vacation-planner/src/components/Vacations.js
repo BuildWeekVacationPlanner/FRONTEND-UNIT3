4bd712ef8bf34a9f44636a5e2208f5d6e75e8665
bodhibrdr <julie.gumerman@gmail.com>
2019-09-20 19:40:28 -0600

    vacations page

--- vacation-planner/src/components/Vacations.js
+++ vacation-planner/src/components/Vacations.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Vacations = () => {
+
+    const [newTrip, setNewTrip] = useState({trip: ""})
+
+    const handleChange = e => {
+        setNewTrip({trip: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(newTrip);
+
+    }
+
     return (
         <div>
             <h2>My Trips</h2>
+            <div>
+                <form onSubmit={e => handleSubmit(e)}>
+                    <label>
+                        Add new trip
+                        <input name="trip" value={newTrip.trip} onChange={e => handleChange(e)}/>
+                    </label>
+                    <button>Create</button>
+                </form>
+            </div>
         </div>
     );
 }
