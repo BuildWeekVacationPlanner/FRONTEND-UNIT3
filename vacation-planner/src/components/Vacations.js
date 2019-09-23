02f2aeb13c2d1569e83a9fc0b68e325aaea1d738
bodhibrdr <julie.gumerman@gmail.com>
2019-09-23 16:05:58 -0600

    updated vacations form

--- vacation-planner/src/components/Vacations.js
+++ vacation-planner/src/components/Vacations.js
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const Vacations = () => {
 
-    const [newTrip, setNewTrip] = useState({trip: ""})
+    const [newTrip, setNewTrip] = useState({title: "", location: "", date: "", description: ""})
 
     const handleChange = e => {
-        setNewTrip({trip: e.target.value})
+        setNewTrip({...newTrip, [e.target.name]: e.target.value})
     }
 
-    const handleSubmit = e => {
+    const handleSubmit = (e, creds) => {
         e.preventDefault();
         console.log(newTrip);
+        axiosWithAuth().post("/vacations/add", creds)
+            .then(res => console.log("Success!"))
+            .catch(err => console.log(err));
 
     }
 
@@ -18,11 +22,41 @@ const Vacations = () => {
         <div>
             <h2>My Trips</h2>
             <div>
-                <form onSubmit={e => handleSubmit(e)}>
-                    <label>
-                        Add new trip
-                        <input name="trip" value={newTrip.trip} onChange={e => handleChange(e)}/>
-                    </label>
+                <form onSubmit={e => handleSubmit(e, newTrip)}>
+    
+                        <h2>Add new trip</h2>
+                        <label>
+                            Title
+                            <input 
+                                name="title" 
+                                value={newTrip.title} 
+                                onChange={e => handleChange(e)}
+                            />
+                        </label>
+                        <label>
+                            Where to?
+                            <input
+                                name="location"
+                                value={newTrip.location}
+                                onChange={e => handleChange(e)}
+                            />
+                        </label>
+                        <label>
+                            Got dates?
+                            <input
+                                name="date"
+                                value={newTrip.date}
+                                onChange={e => handleChange(e)}
+                            />
+                        </label>
+                        <label>
+                            Description
+                            <input
+                                name="description"
+                                value={newTrip.description}
+                                onChange={e => handleChange(e)}
+                            />
+                        </label>
                     <button>+</button>
                 </form>
             </div>
